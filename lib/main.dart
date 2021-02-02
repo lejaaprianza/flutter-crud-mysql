1c51a7abf896ee98ce6eccd58fb672a764e74a08
lejaaprianza <lejaprianza@gmail.com>
2021-02-02 08:30:12 +0700

    menambahkan read mysql

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(new MaterialApp(
+    title: "My Store",
+    home: new Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<List> getData() async {
+    final response = await http.get("http://192.168.43.7/my_store/getdata.php");
+
+    return json.decode(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("My Store"),
+      ),
+      body: new FutureBuilder<List>(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+          return snapshot.hasData
+              ? new ItemList(
+                  list: snapshot.data,
+                )
+              : new Center(
+                  child: new CircularProgressIndicator(),
+                );
+        },
+      ),
+    );
+  }
+}
+
+class ItemList extends StatelessWidget {
+  final List list;
+  ItemList({this.list});
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+      itemCount: list == null ? 0 : list.length,
+      itemBuilder: (context, i) {
+        return new Text(list[i]['item_name']);
+      },
+    );
+  }
+}
